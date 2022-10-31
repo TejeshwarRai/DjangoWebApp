160af634876d425aba05df9d6b4602fbf820bf9f
Tejeshwar Singh Rai <tejeshwarrai@gmail.com>
2022-10-30 20:47:40 -0400

    Added index0.html

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+x
+Hmyapp/migrations/0002_category_warehouse_product_description_and_more.py,8\9\89cfb875caa4139b0fd3ddb029bf3b7540938291
+E
+mysiteF22/settings.py,7\b\7b1147188173c8b3b14eb0b0bc6c66806cd810ed
+A
+mysiteF22/urls.py,b\0\b0f36797735da0cd5f21af492e21397f27ac19a2
--- myapp/views.py
+++ myapp/views.py
@@ -6,39 +6,50 @@ from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
+
+# def index(request):
+#     cat_list = Category.objects.all().order_by('id')[:10]
+#     response = HttpResponse()
+#     heading1 = '<p>' + 'List of categories: ' + '</p>'
+#     response.write(heading1)
+#     for category in cat_list:
+#         para = '<p>'+ str(category.id) + ': ' + str(category) + '</p>'
+#         response.write(para)
+#
+#     prod_list = Product.objects.all().order_by('-price')[:20]
+#     heading2 = '<p>' + 'List of products: ' + '</p>'
+#     response.write(heading2)
+#     for prod in prod_list:
+#         para1 = '<p>' + str(prod.name) + ': $' + str(prod.price) + '</p>'
+#         response.write(para1)
+#
+#     return response
+
 def index(request):
+
     cat_list = Category.objects.all().order_by('id')[:10]
-    response = HttpResponse()
-    heading1 = '<p>' + 'List of categories: ' + '</p>'
-    response.write(heading1)
-    for category in cat_list:
-        para = '<p>'+ str(category.id) + ': ' + str(category) + '</p>'
-        response.write(para)
-
-    prod_list = Product.objects.all().order_by('-price')[:20]
-    heading2 = '<p>' + 'List of products: ' + '</p>'
-    response.write(heading2)
-    for prod in prod_list:
-        para1 = '<p>' + str(prod.name) + ': $' + str(prod.price) + '</p>'
-        response.write(para1)
-
-    return response
-
+    return render(request, 'myapp/index.html', {'cat_list': cat_list})
 
 def about(request):
-    response = HttpResponse()
-    head = '<h1>' + 'This is an Online Store APP.' + '</h1>'
-    response.write(head)
-    return response
+    # response = HttpResponse()
+    # head = '<h1>' + 'This is an Online Store APP.' + '</h1>'
+    # response.write(head)
+    # return response
+    return render(request, 'myapp/about.html')
 
 def detail(request, cat_no) :
+    lis=[]
     response = HttpResponse()
     prod_list = Product.objects.filter(category_id=cat_no)
+    cate = Category.objects.get(id=cat_no).name
+    ware = Category.objects.get(id=cat_no).warehouse
     if (len(prod_list)==0):
         get_object_or_404(Product.objects.filter(category_id=cat_no))
     heading2 = '<p>' + 'List of products: ' + '</p>'
     response.write(heading2)
     for prod in prod_list:
         prodl = '<p>' + str(prod.name) + ': $' + str(prod.price) + '</p>'
+        lis.append(str(prod.name))
         response.write(prodl)
-    return response
+    # return response
+    return render(request, 'myapp/detail.html', {'prod_list': prod_list, 'cate': cate, 'ware': ware})
