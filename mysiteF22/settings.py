a034b4931c2d1d60a1d98b45862b9ed85edbdff2
Tejeshwar Singh Rai <tejeshwarrai@gmail.com>
2022-11-22 11:23:04 -0500

    Update settings.py

--- mysiteF22/settings.py
+++ mysiteF22/settings.py
@@ -126,8 +126,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'uniplaytesting@gmail.com'
-EMAIL_HOST_PASSWORD = 'cdvibpsfhayzbnyv'
+EMAIL_HOST_USER = 'testing@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
